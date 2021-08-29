c89fb41cad02d39c451a9b33cbb9f18ead2bc69a
montechstudiosinc <admin@montech.io>
2021-08-28 20:42:16 -0400

    update

--- src/utils/getTokenSymbol.ts
+++ src/utils/getTokenSymbol.ts
@@ -1,12 +1,19 @@
 import {ChainId, Currency, Token} from "@safemoon/sdk";
 
 export default function getTokenSymbol(token: Token | Currency | undefined, chainId: ChainId | undefined) {
-    return (
-        token?.symbol !== "ETH"
-        ? token?.symbol
-            : chainId === ChainId.BSC_TESTNET || chainId === ChainId.BSC_MAINNET
-            ? "BNB"
-            : "ETH"
+
+    return (token?.symbol !== "PSAFEMOON" || "SAFEMOON" || "ETH"
+    ? token?.symbol
+        : chainId === ChainId.BSC_TESTNET || chainId === ChainId.BSC_MAINNET
+        ? "BNB"
+        : "ETH"
     )
 
+
+
+    
+
 }
+
+
+
