210145a331f2052a0ba13d395e9bc97be007ce6d
bornasamadi <borna@agencywolfe.com>
2021-09-01 19:23:09 +0300

    update :: add token warning modals

---
+++ src/constants/token/warningTokens.ts
@@ -0,0 +1,18 @@
+const warningTokens = {
+  safemoon: {
+    symbol: 'SFM',
+    name: 'SafeMoon',
+    address: '0x8076C74C5e3F5852037F31Ff0093Eeb8c8ADd8D3',
+    chainId: 56,
+    decimals: 9
+  },
+  pSafemoon: {
+    symbol: 'PSAFEMOON',
+    name: 'pSafeMoon',
+    address: '0x16631e53C20Fd2670027C6D53EfE2642929b285C',
+    chainId: 1,
+    decimals: 18
+  }
+}
+
+export default warningTokens
