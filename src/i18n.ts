23a5890330813ac8df29ce13eac1b6a540f9a9e8
Ihor Klymenko <ihorklymenko217@gmail.com>
2021-09-20 14:52:19 -0700

    added a default value that cannot be changed

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,8 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import XHR from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
+const locale = navigator.language;
+
 
 i18next
   .use(XHR)
@@ -19,5 +21,11 @@ i18next
     keySeparator: false,
     interpolation: { escapeValue: false }
   })
+
+localStorage.setItem('i18nextLng', locale);
+window.addEventListener('storage', () => {
+  let storageLng = localStorage.getItem('i18nextLng');
+  if(storageLng !== locale) localStorage.setItem('i18nextLng', locale);
+});
 
 export default i18next
