aab7842f6f84210463b0fcaecc3c53baf3f40273
horray <88070640+phananhvusafemoon@users.noreply.github.com>
2021-12-04 16:29:18 +0700

    Update getTokenSymbol.ts

--- src/utils/getTokenSymbol.ts
+++ src/utils/getTokenSymbol.ts
@@ -4,7 +4,7 @@ import { consolidation } from '../constants'
 export default function getTokenSymbol(token: Token | Currency | undefined, chainId: ChainId | undefined) {
   return token instanceof Token &&
     token.address?.toLowerCase() === consolidation.addresses.v2[chainId as ChainId]?.toLowerCase()
-    ? 'SFM V2'
+    ? 'SFM'
     : token?.symbol !== 'ETH'
     ? token?.symbol
     : chainId === ChainId.BSC_TESTNET || chainId === ChainId.BSC_MAINNET
